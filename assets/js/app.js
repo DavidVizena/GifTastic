51e13e7dfe1219f17932b6f0e414a821ae2c36a2
Vizena2005 <vizena2005@gmail.com>
2018-03-02 17:10:53 -0600

    testing api key working, confirmed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,19 @@
 $(document).ready(function () {
+    // var pieces of API, leaving query on the end of it to make an array
+    var api = "https://api.giphy.com/v1/gifs/";
+    var apiKey = "&api_key=wQgvm3ffxIrfYpTffgpuT9P404Qhn6HI";
+    var limit = "&limit=10";
+    var query = "search?q=ryan+gosling";
+    // var html combines all of the pieces to give you the full HTML link
+    var queryURL = api + query + apiKey + limit;
+    // 
 
-
-
-
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).then(function (response) {
+        console.log(response);
+    });
 
     // document.ready closing
 });
